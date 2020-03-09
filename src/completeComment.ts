5c9d9d526a2f4596991a0c43dd5b83446566548e
Pudding <s.kokuryo@gmail.com>
2020-03-09 15:59:53 +0000

    Implement comment mapper

---
+++ src/completeComment.ts
@@ -0,0 +1,148 @@
+/*
+ * This file is part of textlint-plugin-latex2e
+ *
+ * textlint-plugin-latex2e is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * textlint-plugin-latex2e is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with textlint-plugin-latex2e.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { latexParser } from "latex-utensils";
+import {
+  ASTNodeTypes,
+  AnyTxtNode,
+  TxtNode,
+  TxtParentNode,
+  TxtNodeLineLocation
+} from "@textlint/ast-node-types";
+
+export const convertCommentToTxtNode = (
+  comments: latexParser.Comment[] | undefined | null,
+  rawText: string
+): TxtNode[] => {
+  if (comments === undefined || comments === null) {
+    return [];
+  }
+  return comments.map(
+    (comment: latexParser.Comment): TxtNode => {
+      return {
+        loc: {
+          start: {
+            line: comment.location.start.line,
+            column: comment.location.start.column - 1
+          },
+          end: {
+            line: comment.location.end.line,
+            column: comment.location.end.column - 1
+          }
+        },
+        range: [comment.location.start.offset, comment.location.end.offset],
+        raw: rawText.slice(
+          comment.location.start.offset,
+          comment.location.end.offset
+        ),
+        value: comment.content,
+        type: ASTNodeTypes.Comment
+      };
+    }
+  );
+};
+
+export const isAppearedBeforeNode = (
+  nodeLocation: TxtNodeLineLocation,
+  commentLocation: TxtNodeLineLocation
+): boolean => {
+  if (nodeLocation.start.line >= commentLocation.end.line) {
+    // \begin{document} % comment
+    // \end{document}
+    if (nodeLocation.start.line == commentLocation.end.line) {
+      return nodeLocation.start.column >= commentLocation.end.column;
+    }
+    return true;
+  }
+  return false;
+};
+
+export const isIncludedByNode = (
+  nodeLocation: TxtNodeLineLocation,
+  commentLocation: TxtNodeLineLocation
+): boolean => {
+  if (
+    nodeLocation.start.line <= commentLocation.start.line &&
+    nodeLocation.end.line >= commentLocation.end.line
+  ) {
+    // \begin{document}\end{document} % comment
+    if (nodeLocation.end.line == commentLocation.end.line) {
+      return nodeLocation.end.column > commentLocation.start.column;
+    }
+    return true;
+  }
+  return false;
+};
+
+export const isParentNode = (node: any): node is TxtParentNode => {
+  const children = node.children;
+  return (
+    typeof node == "object" && children !== undefined && Array.isArray(children)
+  );
+};
+
+export const insertComment = (
+  nodes: AnyTxtNode[],
+  comment: TxtNode
+): AnyTxtNode[] => {
+  let isInserted = false;
+  for (let i = 0; i < nodes.length; i++) {
+    // If the comment is appeared before the node, insert it before the node.
+    if (isAppearedBeforeNode(nodes[i].loc, comment.loc)) {
+      nodes.splice(i, 0, comment);
+      isInserted = true;
+      return nodes;
+    }
+    // If the comment is included in the node, try to insert it recursively.
+    if (isIncludedByNode(nodes[i].loc, comment.loc)) {
+      if (isParentNode(nodes[i])) {
+        nodes[i].children = insertComment(nodes[i].children, comment);
+        isInserted = true;
+        return nodes;
+      }
+      // `Parbreak` has no children, even though the range of
+      // `Parbreak` includes the range of comment
+      // if comment is surrounding by the line break.
+      // But the parbreak becomes null, so this condition does not used.
+      throw Error("Unexpected node is given. Is the syntax correct?");
+    }
+  }
+  // If the comment is not inserted, it would be appeared after all nodes.
+  nodes.push(comment);
+  return nodes;
+};
+
+// Mapping all comments to the given AST.
+export const completeComments = (
+  root: any,
+  comments: latexParser.Comment[],
+  rawText: string
+): any => {
+  const textlintComments = convertCommentToTxtNode(comments, rawText);
+  if (isParentNode(root)) {
+    for (const comment of textlintComments) {
+      root.children = insertComment(root.children, comment);
+    }
+    return root;
+  }
+  if (Array.isArray(root)) {
+    for (const comment of textlintComments) {
+      root = insertComment(root, comment);
+    }
+    return root;
+  }
+  throw Error("Given root node is invalid.");
+};
